abb9c29b636380dc0524dbf4c587f69948d6d339
Yan Dou <yan.dou@king.com>
2017-10-31 10:47:49 +0100

    convert to csv

--- simulator_slots.py
+++ simulator_slots.py
@@ -3,6 +3,8 @@ import random
 import config
 import pool
 from pprint import pprint
+import convert_csv
+
 
 class BoxSim():
     def __init__(self):
@@ -12,7 +14,7 @@ class BoxSim():
         self.num_boxes = 0
         self.result = defaultdict(list)
         self.get_reward()
-        #self.get_pool()
+
 
     # def simulate(self): # FIXME, 不同的停止条件
     #     self.simulate_until_pool_empty()
@@ -52,11 +54,8 @@ class BoxSim():
             return False
 
 
-
-    def get_reward(self):
-
+    def get_reward(self):  #draw pieces from the pool
         reward = pool.PoolFactory.create_pool(config.pool_name).random_choice(pick=self.fragments_per_slot)
-        print('reward:', reward)
         return reward
 
 
@@ -64,7 +63,7 @@ class BoxSim():
         for hero_name in reward:
             self.inventory[hero_name] += 1
 
-    def update_result(self, inventory):
+    def update_result(self, inventory):  #sort out reward, create hero,replace duplicates
         for hero_name, num_fragments in inventory.items():
             heroes = []
             for more_heroes in self.result.values():
@@ -72,8 +71,10 @@ class BoxSim():
             if hero_name in heroes:
                 continue
             else:
-                if num_fragments == config.heroes[hero_name]:
+                if num_fragments == self.heroes[hero_name]:
                     self.result[self.num_boxes].append(hero_name)
+
+
 
     def get_result(self):
         return dict(self.result)
@@ -87,21 +88,19 @@ class BoxSim():
 #         result.append(sim.get_result())
 #     return result
 
-def simulate_one_player():
+def simulate_one_player():#simulate every draw, no create hero
     sim_1 = BoxSim()
     sim_2 = BoxSim()
+    sim_3 = BoxSim()
     result = []
-    for num_boxes in range(1, 20): #FIXME stop condition
-        sim_1.simulate_one_box()
-        slot_1 = sim_1.get_result()
-
-        sim_2.simulate_one_box()
-        slot_2 = sim_2.get_result()
-
-        result_all = (num_boxes, slot_1, slot_2)
+    for num_boxes in range(1, 31): #FIXME stop condition
+        slot_1 = sim_1.get_reward()
+        slot_2 = sim_2.get_reward()
+        slot_3 = sim_3.get_reward()
+        result_all = (num_boxes, *slot_1, *slot_2,*slot_3)
         result.append(result_all)
-    #print('result: ', result)
     pprint(result)
+    convert_csv.output_csv_one_player(result)
 
 
 # def main():
